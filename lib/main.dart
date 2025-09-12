1a84c10c542ad657879b4511fb97174abc17a5c0
abdullah2811 <abdullahiar2811@gmail.com>
2025-09-13 00:40:17 +0600

    Day2: Stack, Padding, SizedBox, Icon, ListTile, Print, Wrap; Stateless, Material App, Scafforld (AppBar, BtmNavBar, FloatingActBtn, Drawer, SafeArea)

--- lib/main.dart
+++ lib/main.dart
@@ -4,167 +4,73 @@ void main() {
   runApp(const MyApp());
 }
 
-int number = 42;
-double decimalNumber = 3.14;
-String greeting = "Hello, Flutter!";
-bool isFlutterFun = true;
-List<String> fruits = ["Apple", "Banana", "Cherry"];
-Map<String, dynamic> ages = {
-  "Alice": 30,
-  "Bob": "Twenty-Five",
-  "Charlie": 35.6,
-};
-Set<String> uniqueNames = {"Alice", "Bob", "Charlie"};
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.lightGreenAccent,
+          seedColor: const Color.fromARGB(255, 8, 234, 42),
           brightness: Brightness.dark,
         ),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My First Flutter App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          color: Colors.lime,
-          height: 80,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('My First Flutter App'),
+            centerTitle: true,
+            elevation: 70,
+            //leading: Icon(Icons.construction),
+            actions: [Icon(Icons.exit_to_app), Text('Exit  ')],
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.black,
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                DrawerHeader(child: Text('Flutter App Drawer')),
+                ListTile(title: Text('Home')),
+              ],
+            ),
+          ),
+          floatingActionButton: Row(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Container(
-                alignment: Alignment.center,
-                width: 200,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.redAccent,
-                ),
-                child: Text(
-                  'Red Pill',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
-                ),
+              FloatingActionButton(
+                onPressed: () {
+                  // Action when button is pressed
+                },
+                child: Icon(Icons.add),
               ),
-              Image.asset(
-                'assets/images/pills.jpg',
-                height: 95,
-                width: 150,
-                fit: BoxFit.cover,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: 200,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.blueAccent,
-                ),
-                child: Text(
-                  'Blue Pill',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
-                ),
+              SizedBox(width: 10), // Space between buttons
+              FloatingActionButton(
+                onPressed: () {
+                  // Action when button is pressed
+                },
+                child: Icon(Icons.remove),
               ),
             ],
           ),
+          bottomNavigationBar: NavigationBar(
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+              NavigationDestination(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+            selectedIndex: 0,
+            onDestinationSelected: (int index) {
+              // Handle navigation logic here
+              // print('Selected index: $index');
+            },
+          ),
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: () {
-              // Your second action here
-            },
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-        ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
